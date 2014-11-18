1548035a0ec55d23b85fee06d9e3a53cb08ee2f2
ZJ <zj_jun@163.com>
2014-11-18 22:17:48 +0800

    修改list

--- MyDataStructureAlgorithm/code/platform/common/inc/list.h
+++ MyDataStructureAlgorithm/code/platform/common/inc/list.h
@@ -1,15 +1,40 @@
 /*
  * list.h
  *
- *  Created on: 2014Äê10ÔÂ26ÈÕ
+ *  Created on: 2014ï¿½ï¿½10ï¿½ï¿½26ï¿½ï¿½
  *      Author: ZJ
  */
 
 #ifndef CODE_PLATFORM_COMMON_INC_LIST_H_
 #define CODE_PLATFORM_COMMON_INC_LIST_H_
 
+#include <stdlib.h>
 
+/* Define a structure for linked list elements. */
+typedef struct ListElmt_ {
+	void *data;
+	struct ListElmt_ *next;
+} ListElmt;
 
+/* Define a structure for linked lists. */
+typedef struct List_ {
+	int size;
+	int (*match)(const void *key1, const void *key2);
+	void (*destroy)(void *data);
+	ListElmt *head;
+	ListElmt *tail;
+} List;
 
+/* Public Interface */
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+int list_ins_next(List *list, ListElmt *element, const void *data);
+int list_rem_next(List *list, ListElmt *element, void **data);
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+#define list_is_tail(element) ((element)->next = NULL ? 1 : 0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
 
 #endif /* CODE_PLATFORM_COMMON_INC_LIST_H_ */
