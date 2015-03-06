f6314e9ed34ff3549c443388cf1cb70cc2c8210b
ZJ <zj_jun@163.com>
2015-03-06 23:35:43 +0800

    binary search

---
+++ MyDataStructureAlgorithm/code/platform/common/inc/search.h
@@ -0,0 +1,18 @@
+/*
+ * search.h
+ *
+ *  Created on: Mar 6, 2015
+ *      Author: root
+ */
+
+#ifndef CODE_PLATFORM_COMMON_INC_SEARCH_H_
+#define CODE_PLATFORM_COMMON_INC_SEARCH_H_
+
+int bisearch(void *sorted, void *target, int size, int esize,
+		int (*compare)(const void *key1, const void *key2));
+
+
+
+
+
+#endif /* CODE_PLATFORM_COMMON_INC_SEARCH_H_ */
